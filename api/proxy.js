a3e624fabbd436c8cac4d3b9c3849f4bfdf3fed0
Papryczek <123909104+Papryczekk@users.noreply.github.com>
2025-07-31 23:14:41 +0200

    refactor: update module exports to ES6 syntax and clean up configuration files

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,4 @@
-const fetch = require('node-fetch')
-
-module.exports = async (req, res) => {
+export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
